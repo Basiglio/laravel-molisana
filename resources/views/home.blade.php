2a2804c9883ebe12ef56f35fb9ebfb5458f5b574
Emilio <emilio.furnari87@gmail.com>
2021-02-03 22:02:36 +0100

    secondo push/layout fatto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="{{ asset("css/app.css")}}">
+  <link rel="stylesheet" href="{{asset("css/app.css")}}">
   <title>Document</title>
 </head>
 <body>
   <header>
-    <img src="{{ asset("img/marchio-sito-test.png")}}" alt="">
+    <img src="{{asset("img/marchio-sito-test.png")}}" alt="">
     <ul>
       <li class="active"><a href="#">home</a></li>
       <li><a href="#">prodotti</a></li>
@@ -18,11 +18,35 @@
   </header>
 
   <main>
-    <div class="wrapper80"></div>
+    <div class="wrapper70">
+      <h2>Le Lunghe</h2>
+      <div class="cards">
+        @foreach ($lunghe as $pasta)
+          <div class="card">
+            <img src="{{$pasta["src"]}}" alt="">
+          </div> 
+        @endforeach
+      </div>
+      <h2>Le Corte</h2>
+      <div class="cards">
+        @foreach ($corte as $pasta)
+          <div class="card">
+            <img src="{{$pasta["src"]}}" alt="">
+          </div> 
+        @endforeach
+      </div>
+      <h2>Le Cortissime</h2>
+      <div class="cards">
+        @foreach ($cortissime as $pasta)
+          <div class="card">
+            <img src="{{$pasta["src"]}}" alt="">
+          </div> 
+        @endforeach
+      </div>
+    </div>
   </main>
 
   <footer>
-
   </footer>
 </body>
 </html>
